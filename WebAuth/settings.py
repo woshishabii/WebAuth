b534f2011dfe3d1037247a6f5da6ca0505c3ea3e
woshishabi <51900038+woshishabii@users.noreply.github.com>
2022-04-19 17:28:06 +0800

    Installed Plugins

--- WebAuth/settings.py
+++ WebAuth/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # My Applications
     'authapi',
+
+    # Third Party
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
