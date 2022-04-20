17bc028c50da7db5b4adea9442ba37f995f4de48
woshishabi <51900038+woshishabii@users.noreply.github.com>
2022-04-20 13:22:46 +0800

    Page /about migrated

--- authapi/urls.py
+++ authapi/urls.py
@@ -5,8 +5,9 @@ from . import views
 app_name = 'authapi'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
     path('api/getInstance/<str:query_uuid>/<str:query_species>', views.instance_query, name='instance_query'),
 
     # Testing
-    path('test/base', views.test_base, name='test_base'),
+    # path('test/base', views.test_base, name='test_base'),
 ]
--- authapi/views.py
+++ authapi/views.py
@@ -7,6 +7,10 @@ def index(request):
     return render(request, 'authapi/index.html')
 
 
+def about(request):
+    return render(request, 'authapi/about.html')
+
+
 def instance_query(request, query_uuid, query_species):
     response = json.dumps({"query_uuid": query_uuid, "query_species": query_species})
     return HttpResponse(response)
