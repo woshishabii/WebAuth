b64483b4e407a0eac3994f5c307a84ba832cd113
woshishabi <51900038+woshishabii@users.noreply.github.com>
2022-04-20 12:24:14 +0800

    Query View Comment

--- authapi/views.py
+++ authapi/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 import json
+
+from . import models
 
 
 def index(request):
@@ -8,8 +10,33 @@ def index(request):
 
 
 def instance_query(request, query_uuid, query_species):
-    response = json.dumps({"query_uuid": query_uuid, "query_species": query_species})
-    return HttpResponse(response)
+    """
+    This function is used to query the database for a specific instance of a query.
+    :param request: The request object
+    :param query_uuid: The UUID of the query
+    :param query_species: species of the query
+    :return: HttpResponse
+
+    query_species is a string, separated by '|'.
+    Available species:
+    - name: Instance's description
+    - version: Instance's version
+    - force: If the instance is forced to update
+    - views: Instance's views(If views included, it won't change)
+    - link: New version's download link
+    - announcement: Instance's announcement
+    - update: New version's update log
+    - md5: New version's file's md5
+    """
+
+    query = get_object_or_404(models.Instance, id=query_uuid)
+    # response = json.dumps({"query_uuid": query_uuid, "query_species": query_species})
+    response = {
+        'uuid': str(query.id),
+        'species': query_species,
+        'result': {},
+    }
+    return HttpResponse(json.dumps(response), content_type="application/json")
 
 
 def test_base(request):
