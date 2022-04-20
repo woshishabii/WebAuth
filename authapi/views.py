7c700e377de46340e8aa09477b7eb7c091d4ab3e
woshishabi <51900038+woshishabii@users.noreply.github.com>
2022-04-20 13:13:03 +0800

    Simplest Query Method

--- authapi/views.py
+++ authapi/views.py
@@ -31,11 +31,29 @@ def instance_query(request, query_uuid, query_species):
 
     query = get_object_or_404(models.Instance, id=query_uuid)
     # response = json.dumps({"query_uuid": query_uuid, "query_species": query_species})
+    species = query_species.split('|')
     response = {
         'uuid': str(query.id),
-        'species': query_species,
+        'species': species,
         'result': {},
     }
+    if 'name' in species:
+        response['result']['name'] = query.name
+    if 'version' in species:
+        response['result']['version'] = query.version
+    if 'force' in species:
+        response['result']['force'] = query.force
+    if 'views' in species:
+        response['result']['views'] = query.views
+    if 'link' in species:
+        response['result']['link'] = query.link
+    if 'announcement' in species:
+        response['result']['announcement'] = query.announcement
+    if 'update' in species:
+        response['result']['update'] = query.update
+    if 'md5' in species:
+        response['result']['md5'] = query.md5
+
     return HttpResponse(json.dumps(response), content_type="application/json")
 
 
